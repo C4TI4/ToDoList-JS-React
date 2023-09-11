757ae06c0b7be76fd61320762578692b838e34e4
gjlander <garrett.jlander@gmail.com>
2023-09-11 17:29:50 +0200

    basic structuring of files added

---
+++ script.js
@@ -0,0 +1,11 @@
+//Global variables
+const toDoList = document.querySelector("#toDoList");
+const toDoItem = document.querySelector(".toDoItem");
+
+//Event handler functions
+//  Example: function toDoClickHandler (event) {
+//     code
+// }
+
+//Events
+// Example: toDoList.addEventListener("click", toDoClickHandler);
