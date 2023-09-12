8831603da8c4d83118cd3647fb1dd518564b6ddb
gjlander <garrett.jlander@gmail.com>
2023-09-12 12:04:06 +0200

    todo items are now editable, code is kinda ugly though, wanna make it prettier

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //Global variables
 const toDoList = document.querySelector("#toDoList");
 const toDoItem = document.querySelector(".toDoItem");
+const toDoText = document.querySelector(".toDoText");
 const newItemBtn = document.querySelector("#newItemBtn");
 const textForNewToDo = document.querySelector("#textForNewToDo");
+const textEditor = document.querySelector("#textEditor");
+const editTextPopupWrapper = document.querySelector("#editTextPopupWrapper");
+const editTextBtn = document.querySelector("#editTextBtn");
+const closePopupBtn = document.querySelector("#closePopupBtn");
+const allToDoTexts = document.getElementsByClassName("toDoText");
 
 //Event handler functions
 //  Example: function toDoClickHandler (event) {
@@ -10,29 +16,77 @@ const textForNewToDo = document.querySelector("#textForNewToDo");
 // }
 function newItemBtnClickHandler(e) {
     e.preventDefault();
-    //variables
-    const newToDoItem = document.createElement("li");
-    const newCheckDoneBtn = document.createElement("button");
-    const newToDoText = document.createElement("p");
-    const newDeleteBtn = document.createElement("button");
-    //add appropriate classes
-    newToDoItem.classList.add("toDoItem");
-    newCheckDoneBtn.classList.add("checkDoneBtn");
-    newToDoText.classList.add("toDoText");
-    newDeleteBtn.classList.add("deleteBtn");
-    //add appropriate textContent
-    newCheckDoneBtn.textContent = "□";
-    newToDoText.textContent = `${textForNewToDo.value}`;
-    newDeleteBtn.textContent = "Delete";
-    //append children to li
-    newToDoItem.appendChild(newCheckDoneBtn);
-    newToDoItem.appendChild(newToDoText);
-    newToDoItem.appendChild(newDeleteBtn);
+    if (textForNewToDo.value) {
+        //variables
+        const newToDoItem = document.createElement("li");
+        const newCheckDoneBtn = document.createElement("button");
+        const newToDoText = document.createElement("p");
+        const newDeleteBtn = document.createElement("button");
+        //add appropriate classes
+        newToDoItem.classList.add("toDoItem");
+        newCheckDoneBtn.classList.add("checkDoneBtn");
+        newToDoText.classList.add("toDoText");
+        newDeleteBtn.classList.add("deleteBtn");
+        //add appropriate textContent
+        newCheckDoneBtn.textContent = "□";
+        newToDoText.textContent = `${textForNewToDo.value}`;
+        newDeleteBtn.textContent = "Delete";
+        //append children to li
+        newToDoItem.appendChild(newCheckDoneBtn);
+        newToDoItem.appendChild(newToDoText);
+        newToDoItem.appendChild(newDeleteBtn);
 
-    //append new item to list
-    toDoList.appendChild(newToDoItem);
+        //append new item to list
+        toDoList.appendChild(newToDoItem);
+
+        //update node list so each new one will have the event listener
+        console.log(allToDoTexts);
+        Array.from(allToDoTexts).forEach((toDoText) => {
+            toDoText.addEventListener("dblclick", toDoTextDblClickHandler);
+            toDoText.addEventListener("mouseover", toDoMouseOverHandler);
+            toDoText.addEventListener("mouseout", toDoMouseOutHandler);
+        });
+    }
 }
-
+function toDoTextDblClickHandler(e) {
+    Array.from(allToDoTexts).forEach((toDoText) => {
+        toDoText.removeEventListener("mouseout", toDoMouseOutHandler);
+    });
+    e.target.classList.add("current");
+    console.log(e.target);
+    editTextPopupWrapper.style.display = "flex";
+    textEditor.value = `${this.textContent}`;
+}
+function editTxtBtnClickHandler(e) {
+    e.preventDefault();
+    const currentToDo = document.querySelector(".current");
+    currentToDo.textContent = textEditor.value;
+    Array.from(allToDoTexts).forEach((toDoText) => {
+        toDoText.addEventListener("mouseout", toDoMouseOutHandler);
+    });
+    editTextPopupWrapper.style.display = "none";
+}
+function closePopupBtnClickHandler(e) {
+    e.preventDefault();
+    Array.from(allToDoTexts).forEach((toDoText) => {
+        toDoText.addEventListener("mouseout", toDoMouseOutHandler);
+    });
+    editTextPopupWrapper.style.display = "none";
+}
+function toDoMouseOverHandler(e) {
+    e.target.classList.add("current");
+}
+function toDoMouseOutHandler(e) {
+    e.target.classList.remove("current");
+}
 //Events
 // Example: toDoList.addEventListener("click", toDoClickHandler);
+console.log(allToDoTexts);
 newItemBtn.addEventListener("click", newItemBtnClickHandler);
+Array.from(allToDoTexts).forEach((toDoText) =>
+    toDoText.addEventListener("dblclick", toDoTextDblClickHandler)
+);
+editTextBtn.addEventListener("click", editTxtBtnClickHandler);
+closePopupBtn.addEventListener("click", closePopupBtnClickHandler);
+toDoText.addEventListener("mouseover", toDoMouseOverHandler);
+toDoText.addEventListener("mouseout", toDoMouseOutHandler);
